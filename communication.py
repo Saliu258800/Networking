3e9f6ebf9e88639297cb9cce1b2522d1eee60f5e
Saliu Musah <root@localhost.localdomain>
2025-07-30 17:03:17 +0000

    first commit: mode of communication

---
+++ communication.py
@@ -0,0 +1,19 @@
+# mode of communication 
+
+def main():
+    
+    print("Structure of Shannon-Weaver communication model (mode of communication)")
+    print()
+    communication_mode = [
+        "source",
+        "encoding",
+        "channel(medium)",
+        "decoding",
+        "receiver"
+    ]
+    
+    for m in communication_mode:
+        print("-> ", m, end=" ")
+        
+if __name__ == "__main__":
+    main()
